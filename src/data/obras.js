04501cedce886cfb6e35d9c627a29573fec538b8
Nicole <cnogueira.nicole@gmail.com>
2025-08-04 21:57:19 -0300

    feat: json com as primeiras obras

---
+++ src/data/obras.js
@@ -0,0 +1,55 @@
+import FengSuave from '../assets/ilustration/FengSuave';
+import Hell from '../assets/ilustration/Hell';
+import NaMinhaTerraTemPalmeiras from '../assets/photography/NaMinhaTerraTemPalmeiras';
+import TNDR from '../assets/photography/TNDR';
+
+export const obras = [
+  {
+    id: 1,
+    slug: 'feng-suave',
+    titulo: 'Feng Suave',
+    tecnica: 'Digital illustration',
+    ano: 2025,
+    categoria: 'illustration',
+    ferramentas: ['illutrator', 'Photoshop'],
+    descricao: 'Set of music posters inspired by the aesthetic and mood of the Dutch duo Feng Suave.',
+    tipo: 'imagem',
+    midia: FengSuave
+  },
+  {
+    id: 2,
+    slug: 'hell',
+    titulo: 'Hell',
+    tecnica: 'Digital illustration',
+    ano: 2025,
+    categoria: 'illustration',
+    ferramentas: ['Procreate'],
+    descricao: 'An illustration reimagining Rome in a parallel universe—as if it were hell. This dark, surreal take distorts the city’s iconic architecture and atmosphere, blending historical grandeur with eerie, otherworldly elements to question the fine line between glory and decay.',
+    tipo: 'imagem',
+    midia: Hell
+  },
+  {
+    id: 3,
+    slug: 'tndr',
+    titulo: 'TNDR',
+    tecnica: 'Digital Photography',
+    ano: 2024,
+    categoria: 'photography',
+    ferramentas: ['lightroom', 'photoshop', 'indesign'],
+    descricao: 'Developed for Henrique Bonini’s portfolio, TNDR explores the tension between nature and modernity through brutalist aesthetics and natural tones. Shot at the Calouste Gulbenkian Foundation and in studio using Fujifilm XT-30 II and Nikon D850, the project blends color precision with compositional strength. Post-production and layout were completed using Photoshop, Lightroom, and InDesign. Final images are high-resolution (300 ppi); the printed book measures 210×297 mm.',
+    tipo: 'imagem',
+    midia: TNDR
+  },
+  {
+    id: 4,
+    slug: 'na-minha-terra-tem-palmeiras',
+    titulo: 'Na Minha Terra Tem Palmeiras',
+    tecnica: 'Chemigrams Analog Photography',
+    ano: 2023/24,
+    categoria: 'photography',
+    ferramentas: ['indesign'],
+    descricao: 'This photographic project addresses environmental issues entangled with global power dynamics. Its title references Canção do Exílio by Gonçalves Dias—a Romantic poem evoking Latin American nature and identity. Inspired by the EU–Mercosur trade agreement and its controversial Side Letter, the project critiques Europe’s eco-protectionist stance, which often excludes indigenous voices and favors European economic interests. Shot on 35mm film, the portraits of Latin Americans were deliberately distorted during analog development using substances the EU seeks to restrict—soy, wood, and coffee byproducts. These materials disrupt the image, symbolizing the erasure of Latin American presence. The project also explores experimental photography techniques, including chemical alterations with soybean oil. Final images measure 13×18 cm and were compiled into a printed book designed with Photoshop and InDesign.',
+    tipo: 'imagem',
+    midia: NaMinhaTerraTemPalmeiras
+  }
+];
