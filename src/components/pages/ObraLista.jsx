41697102c02c34726338fe217132cd33c53b5dbe
Nicole <cnogueira.nicole@gmail.com>
2025-08-06 10:25:01 -0300

    fix: ajustes em ObraLista

--- src/components/pages/ObraLista.jsx
+++ src/components/pages/ObraLista.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { obras } from '../../data/obras'; // Nosso "banco de dados"
-import ObraCard from '../ObraCard'; // O card que acabamos de criar
+import { obras } from '../../data/obras';
+import ObraCard from '../ObraCard';
 import styles from './ObraLista.module.css';
 
-// Este componente recebe a 'categoria' que queremos exibir
 function ObraLista({ categoria }) {
-  // Usamos .filter() para criar um novo array apenas com as obras da categoria certa
   const obrasFiltradas = obras.filter(
     (obra) => obra.categoria === categoria
   );
 
   return (
     <div className={styles.listaContainer}>
-      <h1 className={styles.tituloCategoria}>{categoria.replace('-', ' ')}</h1>
       <div className={styles.gridObras}>
         {obrasFiltradas.map((obra) => (
-          // Para cada obra filtrada, renderizamos um ObraCard
           <ObraCard key={obra.id} obra={obra} />
         ))}
       </div>
