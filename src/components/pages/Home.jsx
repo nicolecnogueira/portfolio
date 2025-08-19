cf5770ca93d24d364c44a678d2de5887e5a81e81
Nicole <cnogueira.nicole@gmail.com>
2025-08-19 14:13:34 -0300

    feat: adicionando gif na home no lugar da animação

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Home.module.css';
 import GifHome from '../GifHome';
+import gifHome from '../../assets/gifHome.gif'
 
 function Home() {
     return (
-        <GifHome />
+        <div className={styles.gifContainer}>
+            <img src={gifHome} className={styles.gifImage} />
+        </div>
     );
 }
 
